516912d5103e65b0950586bdc4f9187939c8a371
jenna Kurz <jxk682@miami.edu>
2018-08-26 15:25:29 -0400

    hw updates

---
+++ sketch-2.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(480,240);
+background(204);
+}
+
+function draw() {
+  //head
+  ellipse(200, 80, 130, 70);
+
+//mouth
+ellipse(200,95,70,15);
+
+//left eye
+ellipse(170,47, 15, 15);
+
+//right eye
+  ellipse(230,47, 15, 15);
+
+
+}
