0587d484744a2d40c9f797fc6803c34cd6f2d1dd
jenna Kurz <jxk682@miami.edu>
2018-09-10 12:22:48 -0400

    square

---
+++ week3/squares.js
@@ -0,0 +1,18 @@
+
+var x= 80;
+var y= 40;
+var d= 70;
+var s= 20;
+
+function setup() {
+createCanvas(480, 120);
+background(204);
+
+}
+
+function draw() {
+
+rect (x, y, s, s);
+rect(x+d, y, 2*s, 2*s);
+rect(x+2d, y, 2*s, 2*s);
+}
